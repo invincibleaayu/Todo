0f6bc3943eba2f5663a140508e6ceffce6c127e7
invincibleaayu <giridip862101@gmail.com>
2023-07-13 11:52:26 +0545

    .env file updatd and git ignore maintained

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:aayu@localhost/book"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
